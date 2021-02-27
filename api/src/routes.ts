3d741466b3f2216de27776c2214bc5c69fb8a032
fellipehfa <fellipehf@gmail.com>
2021-02-27 19:55:14 -0300

    day 2

--- api/src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const router = Router();
-
-const UserController = new UserController();
-
-router.post("/users", UserController.create);
-
-export { router };
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,11 @@
 import 'reflect-metadata'
 import express from 'express';
 import "./database";
-import { UserController } from './controllers/UserController';
-import { router } from './routes';
+import { router } from './routes/routes';
 
 const app = express();
 
-app.use(router)
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log('Server is running!🚀⚡'));
