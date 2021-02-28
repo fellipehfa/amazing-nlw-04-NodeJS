e69f5c22e8f851ca6010bbb0a9fd5fa07d9c81e3
fellipehfa <fellipehf@gmail.com>
2021-02-28 09:37:16 -0300

    Surveys

--- api/src/database/migrations/1614478569770-CreateUsers.ts
+++ api/src/database/migrations/1614478569770-CreateUsers.ts
@@ -1,6 +1,6 @@
-import { Column, MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class CreateUsers1614454793662 implements MigrationInterface {
+export class CreateUsers1614478569770 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
